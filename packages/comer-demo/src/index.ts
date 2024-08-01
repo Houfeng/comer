53f20a617e4031b1f79fffda77565f05bc020068
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-01 23:34:47 +0800

    modify demo

--- packages/comer-demo/src/index.ts
+++ packages/comer-demo/src/index.ts
@@ -14,26 +14,26 @@ class ThemeButton extends Component {
 
 class App extends Component {
   mainRef = new Ref<Main>();
+  onButtonClick = () => {
+    console.log("click", this.mainRef.current);
+  };
   build(): Component {
-    return new ThemeProvider({
-      value: 0,
-      children: new Div({
-        children: [
-          new Header({ children: text("Header") }),
-          new Main({ ref: this.mainRef, children: text("Main") }),
-          new Footer({
-            children: [
-              new Button({
-                children: text("Click me"),
-                onClick: (event: MouseEvent) => {
-                  console.log("click", event.target, this.mainRef.current);
-                },
-              }),
-              new ThemeButton(),
-            ],
-          }),
-        ],
-      }),
+    return new Div({
+      children: [
+        new Header({ children: text("Header") }),
+        new Main({
+          ref: this.mainRef,
+          children: [
+            text("Main"),
+            new Button({
+              children: text("Click me"),
+              onClick: this.onButtonClick,
+            }),
+            new ThemeButton(),
+          ],
+        }),
+        new Footer({ children: text("Footer") }),
+      ],
     });
   }
 }
