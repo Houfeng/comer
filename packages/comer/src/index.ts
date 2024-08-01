a480d3f944bc4f0ef4490ff76cab87f70e27e6f5
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-01 19:39:58 +0800

    refine types

--- packages/comer/src/index.ts
+++ packages/comer/src/index.ts
@@ -1,10 +1,13 @@
 export * from "ober";
+
+export * from "./EventUtil";
 export type * from "./TypeUtil";
 
-export * from "./HostAdapter";
 export * from "./Component";
 export * from "./Fragment";
 export * from "./Provider";
-export * from "./HostComponent";
-export * from "./Renderer";
 export * from "./Ref";
+export * from "./Renderer";
+
+export * from "./HostAdapter";
+export * from "./HostComponent";
