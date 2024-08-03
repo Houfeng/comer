09aee573677a59097e30f45deb1fb5223866c23f
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-03 18:53:05 +0800

    widget

--- packages/comer-demo/src/Demo0.ts
+++ packages/comer-demo/src/Demo0.ts
@@ -1,5 +1,12 @@
 import { Component, observable, widget } from "comer";
-import { Button, Text } from "comer-dom";
+import { Button, Div, Text } from "comer-dom";
+
+@widget
+export class Display extends Component<{ value: number }> {
+  build(): Component {
+    return new Text(`Value: ${this.props.value}`);
+  }
+}
 
 export class Demo extends Component {
   state = observable({ value: 0 });
@@ -7,29 +14,15 @@ export class Demo extends Component {
     this.state.value++;
   };
   build() {
-    return new Button({
-      children: new Text(`Click: ${this.state.value}`),
-      onClick: this.onButtonClick,
-      "x-a": "1",
+    return new Div({
+      children: [
+        new Display({ value: this.state.value }),
+        new Button({
+          children: new Text(`Click: ${this.state.value}`),
+          onClick: this.onButtonClick,
+          "x-a": "1",
+        }),
+      ],
     });
   }
 }
-
-@widget
-export class Test extends Component<{ x: 1 }> {
-  static type = "Test";
-  name = "test";
-}
-
-export class SubTest extends Test {
-  sub = 0;
-}
-
-const test = new Test({ x: 1 });
-console.log("test", { test, Test });
-Object.assign(window, { widget, test, Test, SubTest, Component, Demo });
-
-// const subTest = new SubTest({ x: 1 });
-// console.log('subTest', subTest);
-
-// console.log(subTest instanceof Test);
