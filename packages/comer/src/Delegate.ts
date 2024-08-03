1e66d2878c3f1a55212f3f5d318a0c27aaaad2cc
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-03 22:30:17 +0800

    widget => delegate

--- packages/comer/src/Delegate.ts
+++ packages/comer/src/Delegate.ts
@@ -14,9 +14,14 @@ export class Delegate extends Component<any> {
 }
 
 /**
- * Declare a component as a delegate to reduce the cost of re rendering
- * @param Raw component
- * @returns
+ * Declare a component as a delegate to reduce the cost of re rendering.
+ * 
+ * Tips: When a delegated component is inherited, it contains the properties  
+ * and methods of the target component. 
+ * But when creating an instance through new, it is an instance of Delegate
+ * 
+ * @param Target Target component
+ * @returns Delegate component
  */
 export function delegate<T extends ComponentType<any, any>>(Target: T): T {
   const Super = Target as ComponentType<any, any>;
