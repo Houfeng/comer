bcf812e71f8bb26f71b5fa4df0ee3374a1de57f8
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-03 22:19:27 +0800

    widget => delegate

--- packages/comer/src/Delegate.ts
+++ packages/comer/src/Delegate.ts
@@ -4,32 +4,30 @@ import { Component, type ComponentType } from "./Component";
 export class Delegate extends Component<any> {
   constructor(
     props: any,
-    public Raw: ComponentType<any, any>,
+    private Target: ComponentType<any, any>,
   ) {
     super(props);
   }
   build(): Component {
-    return new this.Raw(this.props);
+    return new this.Target(this.props);
   }
 }
 
 /**
- * Declare a component as a widget to reduce the cost of re rendering
+ * Declare a component as a delegate to reduce the cost of re rendering
  * @param Raw component
  * @returns
  */
-export function delegate<T extends ComponentType<any, any>>(
-  RawComponent: T,
-): T {
-  const Super = RawComponent as ComponentType<any, any>;
+export function delegate<T extends ComponentType<any, any>>(Target: T): T {
+  const Super = Target as ComponentType<any, any>;
   class Wrapper extends Super {
     constructor(props: ConstructorParameters<T>[0]) {
       if (!new.target || new.target === Wrapper) {
-        return new Delegate(props, RawComponent);
+        return new Delegate(props, Target);
       }
       super(props);
     }
   }
-  Object.defineProperty(Wrapper, "name", { value: RawComponent.name });
+  Object.defineProperty(Wrapper, "name", { value: Target.name });
   return Wrapper as T;
 }
