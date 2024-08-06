0a217b1c2324d49140b31941b9659ad7c4f9f4fe
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-06 23:31:43 +0800

    add comments

--- packages/comer/src/HostAdapter.ts
+++ packages/comer/src/HostAdapter.ts
@@ -18,13 +18,79 @@ export type HostElementEvents = Record<string, HostEventListener>;
  * @interface
  */
 export interface HostAdapter<E extends HostElement> {
+  /**
+   * Check if it is a host element
+   * @param value
+   * @returns {boolean}
+   */
   isHostElement(value: unknown): value is E;
+
+  /**
+   * Create a host element
+   * @param type
+   * @returns {HostElement}
+   */
   createElement(type: string): E;
+
+  /**
+   * Remove a host element
+   * @param element The element to be removed
+   * @returns {void}
+   */
   removeElement(element: E): void;
+
+  /**
+   * Insert a host element
+   *
+   * if (!anchor) :
+   *   Append to the last child level of the parent element.
+   *
+   * if (isHostElement(anchor)) :
+   *   After inserting into the anchor element.
+   *
+   * if (isString(anchor)) :
+   *   Processed by adapter based on anchor value,
+   *   usually processed according to fragment mapping configuration,
+   *   currently no need to process.
+   *
+   * @param parent Parent element
+   * @param element The element to be inserted
+   * @param anchor Anchor position for insertion
+   * @returns {void}
+   */
   insertElement(parent: E, element: E, anchor?: E | string): void;
+
+  /**
+   * Update a set of properties of the host element,
+   * reset when the property value is null.
+   * @param element Target element
+   * @param props Props key/value record
+   */
   updateProps(element: E, props: HostElementProps): void;
+
+  /**
+   * Bind multiple event handling functions
+   * @param element Target element
+   * @param events Events key/value record
+   */
   attachEvents(element: E, events: HostElementEvents): void;
+
+  /**
+   * Unbind multiple event handling functions
+   * @param element Target element
+   * @param events Events key/value record
+   */
   removeEvents(element: E, events: HostElementEvents): void;
+
+  /**
+   * Add a change and rendering task to the host's main loop
+   * @param handler
+   */
   requestHostCallback(handler: (time: number) => void): unknown;
+
+  /**
+   * Cancel a change and rendering task from the host's event loop
+   * @param id Task id
+   */
   cancelHostCallback(id: unknown): void;
 }
--- packages/comer/src/Scheduler.ts
+++ packages/comer/src/Scheduler.ts
@@ -5,5 +5,5 @@ export class Scheduler<T extends HostAdapter<HostElement>> {
    * Create a comer scheduler instance using the specified adapter
    * @param adapter Host adapter (eg. DOMAdapter)
    */
-  constructor(protected adapter: T) { }
+  constructor(protected adapter: T) {}
 }
