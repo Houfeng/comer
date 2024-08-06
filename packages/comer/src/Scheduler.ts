2080c73b5ba3c5f0aaa899d44834ebdc9aa11cb9
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-06 23:07:29 +0800

    Scheduler

---
+++ packages/comer/src/Scheduler.ts
@@ -0,0 +1,9 @@
+import { HostAdapter, HostElement } from "./HostAdapter";
+
+export class Scheduler<T extends HostAdapter<HostElement>> {
+  /**
+   * Create a comer scheduler instance using the specified adapter
+   * @param adapter Host adapter (eg. DOMAdapter)
+   */
+  constructor(protected adapter: T) { }
+}
