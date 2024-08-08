4b7b19c488ffc70379106c19757ba16daa40deaa
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-08 21:03:23 +0800

    comer-dom generate all components

--- packages/comer-dom/src/components/HTML.generated.ts
+++ packages/comer-dom/src/components/HTML.generated.ts
@@ -1,342 +1,114 @@
 import { DOMComponent } from "../DOMComponent";
 
-export class A extends DOMComponent<HTMLAnchorElement, {}, A> {
-  type = "a";
-}
-export class Abbr extends DOMComponent<HTMLElement, {}, Abbr> {
-  type = "abbr";
-}
-export class Address extends DOMComponent<HTMLElement, {}, Address> {
-  type = "address";
-}
-export class Area extends DOMComponent<HTMLAreaElement, {}, Area> {
-  type = "area";
-}
-export class Article extends DOMComponent<HTMLElement, {}, Article> {
-  type = "article";
-}
-export class Aside extends DOMComponent<HTMLElement, {}, Aside> {
-  type = "aside";
-}
-export class Audio extends DOMComponent<HTMLAudioElement, {}, Audio> {
-  type = "audio";
-}
-export class B extends DOMComponent<HTMLElement, {}, B> {
-  type = "b";
-}
-export class Base extends DOMComponent<HTMLBaseElement, {}, Base> {
-  type = "base";
-}
-export class Bdi extends DOMComponent<HTMLElement, {}, Bdi> {
-  type = "bdi";
-}
-export class Bdo extends DOMComponent<HTMLElement, {}, Bdo> {
-  type = "bdo";
-}
-export class Blockquote extends DOMComponent<HTMLQuoteElement, {}, Blockquote> {
-  type = "blockquote";
-}
-export class Body extends DOMComponent<HTMLBodyElement, {}, Body> {
-  type = "body";
-}
-export class Br extends DOMComponent<HTMLBRElement, {}, Br> {
-  type = "br";
-}
-export class Button extends DOMComponent<HTMLButtonElement, {}, Button> {
-  type = "button";
-}
-export class Canvas extends DOMComponent<HTMLCanvasElement, {}, Canvas> {
-  type = "canvas";
-}
-export class Caption extends DOMComponent<
-  HTMLTableCaptionElement,
-  {},
-  Caption
-> {
-  type = "caption";
-}
-export class Cite extends DOMComponent<HTMLElement, {}, Cite> {
-  type = "cite";
-}
-export class Code extends DOMComponent<HTMLElement, {}, Code> {
-  type = "code";
-}
-export class Col extends DOMComponent<HTMLTableColElement, {}, Col> {
-  type = "col";
-}
-export class Colgroup extends DOMComponent<HTMLTableColElement, {}, Colgroup> {
-  type = "colgroup";
-}
-export class Data extends DOMComponent<HTMLDataElement, {}, Data> {
-  type = "data";
-}
-export class Datalist extends DOMComponent<HTMLDataListElement, {}, Datalist> {
-  type = "datalist";
-}
-export class Dd extends DOMComponent<HTMLElement, {}, Dd> {
-  type = "dd";
-}
-export class Del extends DOMComponent<HTMLModElement, {}, Del> {
-  type = "del";
-}
-export class Details extends DOMComponent<HTMLDetailsElement, {}, Details> {
-  type = "details";
-}
-export class Dfn extends DOMComponent<HTMLElement, {}, Dfn> {
-  type = "dfn";
-}
-export class Dialog extends DOMComponent<HTMLDialogElement, {}, Dialog> {
-  type = "dialog";
-}
-export class Div extends DOMComponent<HTMLDivElement, {}, Div> {
-  type = "div";
-}
-export class Dl extends DOMComponent<HTMLDListElement, {}, Dl> {
-  type = "dl";
-}
-export class Dt extends DOMComponent<HTMLElement, {}, Dt> {
-  type = "dt";
-}
-export class Em extends DOMComponent<HTMLElement, {}, Em> {
-  type = "em";
-}
-export class Embed extends DOMComponent<HTMLEmbedElement, {}, Embed> {
-  type = "embed";
-}
-export class Fieldset extends DOMComponent<HTMLFieldSetElement, {}, Fieldset> {
-  type = "fieldset";
-}
-export class Figcaption extends DOMComponent<HTMLElement, {}, Figcaption> {
-  type = "figcaption";
-}
-export class Figure extends DOMComponent<HTMLElement, {}, Figure> {
-  type = "figure";
-}
-export class Footer extends DOMComponent<HTMLElement, {}, Footer> {
-  type = "footer";
-}
-export class Form extends DOMComponent<HTMLFormElement, {}, Form> {
-  type = "form";
-}
-export class H1 extends DOMComponent<HTMLHeadingElement, {}, H1> {
-  type = "h1";
-}
-export class H2 extends DOMComponent<HTMLHeadingElement, {}, H2> {
-  type = "h2";
-}
-export class H3 extends DOMComponent<HTMLHeadingElement, {}, H3> {
-  type = "h3";
-}
-export class H4 extends DOMComponent<HTMLHeadingElement, {}, H4> {
-  type = "h4";
-}
-export class H5 extends DOMComponent<HTMLHeadingElement, {}, H5> {
-  type = "h5";
-}
-export class H6 extends DOMComponent<HTMLHeadingElement, {}, H6> {
-  type = "h6";
-}
-export class Head extends DOMComponent<HTMLHeadElement, {}, Head> {
-  type = "head";
-}
-export class Header extends DOMComponent<HTMLElement, {}, Header> {
-  type = "header";
-}
-export class Hgroup extends DOMComponent<HTMLElement, {}, Hgroup> {
-  type = "hgroup";
-}
-export class Hr extends DOMComponent<HTMLHRElement, {}, Hr> {
-  type = "hr";
-}
-export class Html extends DOMComponent<HTMLHtmlElement, {}, Html> {
-  type = "html";
-}
-export class I extends DOMComponent<HTMLElement, {}, I> {
-  type = "i";
-}
-export class Iframe extends DOMComponent<HTMLIFrameElement, {}, Iframe> {
-  type = "iframe";
-}
-export class Img extends DOMComponent<HTMLImageElement, {}, Img> {
-  type = "img";
-}
-export class Input extends DOMComponent<HTMLInputElement, {}, Input> {
-  type = "input";
-}
-export class Ins extends DOMComponent<HTMLModElement, {}, Ins> {
-  type = "ins";
-}
-export class Kbd extends DOMComponent<HTMLElement, {}, Kbd> {
-  type = "kbd";
-}
-export class Label extends DOMComponent<HTMLLabelElement, {}, Label> {
-  type = "label";
-}
-export class Legend extends DOMComponent<HTMLLegendElement, {}, Legend> {
-  type = "legend";
-}
-export class Li extends DOMComponent<HTMLLIElement, {}, Li> {
-  type = "li";
-}
-export class Link extends DOMComponent<HTMLLinkElement, {}, Link> {
-  type = "link";
-}
-export class Main extends DOMComponent<HTMLElement, {}, Main> {
-  type = "main";
-}
-export class Map extends DOMComponent<HTMLMapElement, {}, Map> {
-  type = "map";
-}
-export class Mark extends DOMComponent<HTMLElement, {}, Mark> {
-  type = "mark";
-}
-export class Menu extends DOMComponent<HTMLMenuElement, {}, Menu> {
-  type = "menu";
-}
-export class Meta extends DOMComponent<HTMLMetaElement, {}, Meta> {
-  type = "meta";
-}
-export class Meter extends DOMComponent<HTMLMeterElement, {}, Meter> {
-  type = "meter";
-}
-export class Nav extends DOMComponent<HTMLElement, {}, Nav> {
-  type = "nav";
-}
-export class Noscript extends DOMComponent<HTMLElement, {}, Noscript> {
-  type = "noscript";
-}
-export class Object extends DOMComponent<HTMLObjectElement, {}, Object> {
-  type = "object";
-}
-export class Ol extends DOMComponent<HTMLOListElement, {}, Ol> {
-  type = "ol";
-}
-export class Optgroup extends DOMComponent<HTMLOptGroupElement, {}, Optgroup> {
-  type = "optgroup";
-}
-export class Option extends DOMComponent<HTMLOptionElement, {}, Option> {
-  type = "option";
-}
-export class Output extends DOMComponent<HTMLOutputElement, {}, Output> {
-  type = "output";
-}
-export class P extends DOMComponent<HTMLParagraphElement, {}, P> {
-  type = "p";
-}
-export class Picture extends DOMComponent<HTMLPictureElement, {}, Picture> {
-  type = "picture";
-}
-export class Pre extends DOMComponent<HTMLPreElement, {}, Pre> {
-  type = "pre";
-}
-export class Progress extends DOMComponent<HTMLProgressElement, {}, Progress> {
-  type = "progress";
-}
-export class Q extends DOMComponent<HTMLQuoteElement, {}, Q> {
-  type = "q";
-}
-export class Rp extends DOMComponent<HTMLElement, {}, Rp> {
-  type = "rp";
-}
-export class Rt extends DOMComponent<HTMLElement, {}, Rt> {
-  type = "rt";
-}
-export class Ruby extends DOMComponent<HTMLElement, {}, Ruby> {
-  type = "ruby";
-}
-export class S extends DOMComponent<HTMLElement, {}, S> {
-  type = "s";
-}
-export class Samp extends DOMComponent<HTMLElement, {}, Samp> {
-  type = "samp";
-}
-export class Script extends DOMComponent<HTMLScriptElement, {}, Script> {
-  type = "script";
-}
-export class Search extends DOMComponent<HTMLElement, {}, Search> {
-  type = "search";
-}
-export class Section extends DOMComponent<HTMLElement, {}, Section> {
-  type = "section";
-}
-export class Select extends DOMComponent<HTMLSelectElement, {}, Select> {
-  type = "select";
-}
-export class Slot extends DOMComponent<HTMLSlotElement, {}, Slot> {
-  type = "slot";
-}
-export class Small extends DOMComponent<HTMLElement, {}, Small> {
-  type = "small";
-}
-export class Source extends DOMComponent<HTMLSourceElement, {}, Source> {
-  type = "source";
-}
-export class Span extends DOMComponent<HTMLSpanElement, {}, Span> {
-  type = "span";
-}
-export class Strong extends DOMComponent<HTMLElement, {}, Strong> {
-  type = "strong";
-}
-export class Style extends DOMComponent<HTMLStyleElement, {}, Style> {
-  type = "style";
-}
-export class Sub extends DOMComponent<HTMLElement, {}, Sub> {
-  type = "sub";
-}
-export class Summary extends DOMComponent<HTMLElement, {}, Summary> {
-  type = "summary";
-}
-export class Sup extends DOMComponent<HTMLElement, {}, Sup> {
-  type = "sup";
-}
-export class Table extends DOMComponent<HTMLTableElement, {}, Table> {
-  type = "table";
-}
-export class Tbody extends DOMComponent<HTMLTableSectionElement, {}, Tbody> {
-  type = "tbody";
-}
-export class Td extends DOMComponent<HTMLTableCellElement, {}, Td> {
-  type = "td";
-}
-export class Template extends DOMComponent<HTMLTemplateElement, {}, Template> {
-  type = "template";
-}
-export class Textarea extends DOMComponent<HTMLTextAreaElement, {}, Textarea> {
-  type = "textarea";
-}
-export class Tfoot extends DOMComponent<HTMLTableSectionElement, {}, Tfoot> {
-  type = "tfoot";
-}
-export class Th extends DOMComponent<HTMLTableCellElement, {}, Th> {
-  type = "th";
-}
-export class Thead extends DOMComponent<HTMLTableSectionElement, {}, Thead> {
-  type = "thead";
-}
-export class Time extends DOMComponent<HTMLTimeElement, {}, Time> {
-  type = "time";
-}
-export class Title extends DOMComponent<HTMLTitleElement, {}, Title> {
-  type = "title";
-}
-export class Tr extends DOMComponent<HTMLTableRowElement, {}, Tr> {
-  type = "tr";
-}
-export class Track extends DOMComponent<HTMLTrackElement, {}, Track> {
-  type = "track";
-}
-export class U extends DOMComponent<HTMLElement, {}, U> {
-  type = "u";
-}
-export class Ul extends DOMComponent<HTMLUListElement, {}, Ul> {
-  type = "ul";
-}
-export class Var extends DOMComponent<HTMLElement, {}, Var> {
-  type = "var";
-}
-export class Video extends DOMComponent<HTMLVideoElement, {}, Video> {
-  type = "video";
-}
-export class Wbr extends DOMComponent<HTMLElement, {}, Wbr> {
-  type = "wbr";
-}
+export class A extends DOMComponent<HTMLAnchorElement, {}, A> { type="a" }
+export class Abbr extends DOMComponent<HTMLElement, {}, Abbr> { type="abbr" }
+export class Address extends DOMComponent<HTMLElement, {}, Address> { type="address" }
+export class Area extends DOMComponent<HTMLAreaElement, {}, Area> { type="area" }
+export class Article extends DOMComponent<HTMLElement, {}, Article> { type="article" }
+export class Aside extends DOMComponent<HTMLElement, {}, Aside> { type="aside" }
+export class Audio extends DOMComponent<HTMLAudioElement, {}, Audio> { type="audio" }
+export class B extends DOMComponent<HTMLElement, {}, B> { type="b" }
+export class Base extends DOMComponent<HTMLBaseElement, {}, Base> { type="base" }
+export class Bdi extends DOMComponent<HTMLElement, {}, Bdi> { type="bdi" }
+export class Bdo extends DOMComponent<HTMLElement, {}, Bdo> { type="bdo" }
+export class Blockquote extends DOMComponent<HTMLQuoteElement, {}, Blockquote> { type="blockquote" }
+export class Body extends DOMComponent<HTMLBodyElement, {}, Body> { type="body" }
+export class Br extends DOMComponent<HTMLBRElement, {}, Br> { type="br" }
+export class Button extends DOMComponent<HTMLButtonElement, {}, Button> { type="button" }
+export class Canvas extends DOMComponent<HTMLCanvasElement, {}, Canvas> { type="canvas" }
+export class Caption extends DOMComponent<HTMLTableCaptionElement, {}, Caption> { type="caption" }
+export class Cite extends DOMComponent<HTMLElement, {}, Cite> { type="cite" }
+export class Code extends DOMComponent<HTMLElement, {}, Code> { type="code" }
+export class Col extends DOMComponent<HTMLTableColElement, {}, Col> { type="col" }
+export class Colgroup extends DOMComponent<HTMLTableColElement, {}, Colgroup> { type="colgroup" }
+export class Data extends DOMComponent<HTMLDataElement, {}, Data> { type="data" }
+export class Datalist extends DOMComponent<HTMLDataListElement, {}, Datalist> { type="datalist" }
+export class Dd extends DOMComponent<HTMLElement, {}, Dd> { type="dd" }
+export class Del extends DOMComponent<HTMLModElement, {}, Del> { type="del" }
+export class Details extends DOMComponent<HTMLDetailsElement, {}, Details> { type="details" }
+export class Dfn extends DOMComponent<HTMLElement, {}, Dfn> { type="dfn" }
+export class Dialog extends DOMComponent<HTMLDialogElement, {}, Dialog> { type="dialog" }
+export class Div extends DOMComponent<HTMLDivElement, {}, Div> { type="div" }
+export class Dl extends DOMComponent<HTMLDListElement, {}, Dl> { type="dl" }
+export class Dt extends DOMComponent<HTMLElement, {}, Dt> { type="dt" }
+export class Em extends DOMComponent<HTMLElement, {}, Em> { type="em" }
+export class Embed extends DOMComponent<HTMLEmbedElement, {}, Embed> { type="embed" }
+export class Fieldset extends DOMComponent<HTMLFieldSetElement, {}, Fieldset> { type="fieldset" }
+export class Figcaption extends DOMComponent<HTMLElement, {}, Figcaption> { type="figcaption" }
+export class Figure extends DOMComponent<HTMLElement, {}, Figure> { type="figure" }
+export class Footer extends DOMComponent<HTMLElement, {}, Footer> { type="footer" }
+export class Form extends DOMComponent<HTMLFormElement, {}, Form> { type="form" }
+export class H1 extends DOMComponent<HTMLHeadingElement, {}, H1> { type="h1" }
+export class H2 extends DOMComponent<HTMLHeadingElement, {}, H2> { type="h2" }
+export class H3 extends DOMComponent<HTMLHeadingElement, {}, H3> { type="h3" }
+export class H4 extends DOMComponent<HTMLHeadingElement, {}, H4> { type="h4" }
+export class H5 extends DOMComponent<HTMLHeadingElement, {}, H5> { type="h5" }
+export class H6 extends DOMComponent<HTMLHeadingElement, {}, H6> { type="h6" }
+export class Head extends DOMComponent<HTMLHeadElement, {}, Head> { type="head" }
+export class Header extends DOMComponent<HTMLElement, {}, Header> { type="header" }
+export class Hgroup extends DOMComponent<HTMLElement, {}, Hgroup> { type="hgroup" }
+export class Hr extends DOMComponent<HTMLHRElement, {}, Hr> { type="hr" }
+export class Html extends DOMComponent<HTMLHtmlElement, {}, Html> { type="html" }
+export class I extends DOMComponent<HTMLElement, {}, I> { type="i" }
+export class Iframe extends DOMComponent<HTMLIFrameElement, {}, Iframe> { type="iframe" }
+export class Img extends DOMComponent<HTMLImageElement, {}, Img> { type="img" }
+export class Input extends DOMComponent<HTMLInputElement, {}, Input> { type="input" }
+export class Ins extends DOMComponent<HTMLModElement, {}, Ins> { type="ins" }
+export class Kbd extends DOMComponent<HTMLElement, {}, Kbd> { type="kbd" }
+export class Label extends DOMComponent<HTMLLabelElement, {}, Label> { type="label" }
+export class Legend extends DOMComponent<HTMLLegendElement, {}, Legend> { type="legend" }
+export class Li extends DOMComponent<HTMLLIElement, {}, Li> { type="li" }
+export class Link extends DOMComponent<HTMLLinkElement, {}, Link> { type="link" }
+export class Main extends DOMComponent<HTMLElement, {}, Main> { type="main" }
+export class Map extends DOMComponent<HTMLMapElement, {}, Map> { type="map" }
+export class Mark extends DOMComponent<HTMLElement, {}, Mark> { type="mark" }
+export class Menu extends DOMComponent<HTMLMenuElement, {}, Menu> { type="menu" }
+export class Meta extends DOMComponent<HTMLMetaElement, {}, Meta> { type="meta" }
+export class Meter extends DOMComponent<HTMLMeterElement, {}, Meter> { type="meter" }
+export class Nav extends DOMComponent<HTMLElement, {}, Nav> { type="nav" }
+export class Noscript extends DOMComponent<HTMLElement, {}, Noscript> { type="noscript" }
+export class Object extends DOMComponent<HTMLObjectElement, {}, Object> { type="object" }
+export class Ol extends DOMComponent<HTMLOListElement, {}, Ol> { type="ol" }
+export class Optgroup extends DOMComponent<HTMLOptGroupElement, {}, Optgroup> { type="optgroup" }
+export class Option extends DOMComponent<HTMLOptionElement, {}, Option> { type="option" }
+export class Output extends DOMComponent<HTMLOutputElement, {}, Output> { type="output" }
+export class P extends DOMComponent<HTMLParagraphElement, {}, P> { type="p" }
+export class Picture extends DOMComponent<HTMLPictureElement, {}, Picture> { type="picture" }
+export class Pre extends DOMComponent<HTMLPreElement, {}, Pre> { type="pre" }
+export class Progress extends DOMComponent<HTMLProgressElement, {}, Progress> { type="progress" }
+export class Q extends DOMComponent<HTMLQuoteElement, {}, Q> { type="q" }
+export class Rp extends DOMComponent<HTMLElement, {}, Rp> { type="rp" }
+export class Rt extends DOMComponent<HTMLElement, {}, Rt> { type="rt" }
+export class Ruby extends DOMComponent<HTMLElement, {}, Ruby> { type="ruby" }
+export class S extends DOMComponent<HTMLElement, {}, S> { type="s" }
+export class Samp extends DOMComponent<HTMLElement, {}, Samp> { type="samp" }
+export class Script extends DOMComponent<HTMLScriptElement, {}, Script> { type="script" }
+export class Search extends DOMComponent<HTMLElement, {}, Search> { type="search" }
+export class Section extends DOMComponent<HTMLElement, {}, Section> { type="section" }
+export class Select extends DOMComponent<HTMLSelectElement, {}, Select> { type="select" }
+export class Slot extends DOMComponent<HTMLSlotElement, {}, Slot> { type="slot" }
+export class Small extends DOMComponent<HTMLElement, {}, Small> { type="small" }
+export class Source extends DOMComponent<HTMLSourceElement, {}, Source> { type="source" }
+export class Span extends DOMComponent<HTMLSpanElement, {}, Span> { type="span" }
+export class Strong extends DOMComponent<HTMLElement, {}, Strong> { type="strong" }
+export class Style extends DOMComponent<HTMLStyleElement, {}, Style> { type="style" }
+export class Sub extends DOMComponent<HTMLElement, {}, Sub> { type="sub" }
+export class Summary extends DOMComponent<HTMLElement, {}, Summary> { type="summary" }
+export class Sup extends DOMComponent<HTMLElement, {}, Sup> { type="sup" }
+export class Table extends DOMComponent<HTMLTableElement, {}, Table> { type="table" }
+export class Tbody extends DOMComponent<HTMLTableSectionElement, {}, Tbody> { type="tbody" }
+export class Td extends DOMComponent<HTMLTableCellElement, {}, Td> { type="td" }
+export class Template extends DOMComponent<HTMLTemplateElement, {}, Template> { type="template" }
+export class Textarea extends DOMComponent<HTMLTextAreaElement, {}, Textarea> { type="textarea" }
+export class Tfoot extends DOMComponent<HTMLTableSectionElement, {}, Tfoot> { type="tfoot" }
+export class Th extends DOMComponent<HTMLTableCellElement, {}, Th> { type="th" }
+export class Thead extends DOMComponent<HTMLTableSectionElement, {}, Thead> { type="thead" }
+export class Time extends DOMComponent<HTMLTimeElement, {}, Time> { type="time" }
+export class Title extends DOMComponent<HTMLTitleElement, {}, Title> { type="title" }
+export class Tr extends DOMComponent<HTMLTableRowElement, {}, Tr> { type="tr" }
+export class Track extends DOMComponent<HTMLTrackElement, {}, Track> { type="track" }
+export class U extends DOMComponent<HTMLElement, {}, U> { type="u" }
+export class Ul extends DOMComponent<HTMLUListElement, {}, Ul> { type="ul" }
+export class Var extends DOMComponent<HTMLElement, {}, Var> { type="var" }
+export class Video extends DOMComponent<HTMLVideoElement, {}, Video> { type="video" }
+export class Wbr extends DOMComponent<HTMLElement, {}, Wbr> { type="wbr" }
