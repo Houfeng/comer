f18bd86e83e9c8d51be6f7587096a420941361f4
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-12 12:53:46 +0800

    add comments

--- packages/comer/src/HostComponent.ts
+++ packages/comer/src/HostComponent.ts
@@ -12,13 +12,12 @@ export abstract class HostComponent<
   TProps extends object = {},
   TElement extends HostElement = HostElement,
 > extends Component<TProps, TElement> {
-
   /**
    * Mapping to the type identifier of the host native element
-   * 
-   * Tips: If using TypeScript, it should be declared as read-only 
+   *
+   * Tips: If using TypeScript, it should be declared as read-only
    *       to ensure that it is not accidentally modified
-   * 
+   *
    */
   static readonly type?: string;
 
