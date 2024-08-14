82383a556f25dc482f116ecb728385ef5fb5b1a2
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2024-08-15 07:05:49 +0800

    update demo

---
+++ packages/comer-demo/src/Demo4.ts
@@ -0,0 +1,39 @@
+import { Component, Deferrable, delegate, Fragment, observable } from "comer";
+import { Button, Div, StyleClass, styled } from "comer-dom";
+
+const button_class = StyleClass({
+  padding: "8px 16px",
+});
+
+const StyledButtonBase = styled(Button, {
+  $extends: button_class,
+  color: "#333",
+  margin: "4px",
+});
+
+const StyledButton = styled(StyledButtonBase, {
+  color: "red",
+});
+
+@delegate
+export class Demo extends Component {
+  state = observable({ value: 0 });
+  onBtnClick = () => {
+    this.state.value++;
+  };
+  build() {
+    const { value } = this.state;
+    return new Div({
+      children: [
+        new Fragment([
+          new Deferrable([
+            new StyledButton({ innerText: "M1" }),
+            new StyledButton({ innerText: "M2" }),
+          ]),
+        ]),
+        new StyledButton({ innerText: String(value) }),
+        new StyledButton({ innerText: "Click", onClick: this.onBtnClick }),
+      ],
+    });
+  }
+}
