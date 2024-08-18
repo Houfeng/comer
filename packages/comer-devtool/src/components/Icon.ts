933855c5dad72e85703cc9928ecf313d9fd98dcc
zhanfeng.hzf <admin@xhou.net>
2024-08-18 11:21:41 +0800

    refine devtool

--- packages/comer-devtool/src/components/Icon.ts
+++ packages/comer-devtool/src/components/Icon.ts
@@ -1,7 +1,7 @@
 import { Component } from "comer";
 import { I, styled } from "comer-dom";
 
-const IWrapper = styled(I, {
+const IconWrapper = styled(I, {
   verticalAlign: "middle",
   width: "16px",
   height: "16px",
@@ -19,7 +19,7 @@ export class Icon extends Component<{
 }> {
   build(): Component {
     const { name, checked = false, onClick } = this.props;
-    return new IWrapper({
+    return new IconWrapper({
       className: `comer comer-${name}`,
       onClick,
       "data-checked": String(checked),
