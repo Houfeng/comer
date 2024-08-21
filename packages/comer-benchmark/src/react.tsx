0460cc27fd01a702aadcb2c62edacf8e801b0c7e
zhanfeng.hzf <admin@xhou.net>
2024-08-21 14:16:44 +0800

    benchmark

--- packages/comer-benchmark/src/react.tsx
+++ packages/comer-benchmark/src/react.tsx
@@ -1,9 +1,30 @@
-import * as React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
-function App() {
-  return (<div>React demo</div>)
+function Item(props: { text: string }) {
+  return <span style={{ padding: 4 }}>{props.text}</span>
+}
+
+function List() {
+  const [num, setNum] = useState(100);
+  const [value, setValue] = useState(1);
+  const updateNum = (event: ChangeEvent<HTMLInputElement>) => {
+    setNum(Number(event.target.value))
+  }
+  const updateValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(Number(event.target.value))
+  }
+  return (
+    <div>
+      <input value={num} onChange={updateNum} />
+      <input value={value} onChange={updateValue} />
+      <div style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}>
+        {new Array(num).fill(' ')
+          .map((_, i) => <Item text={`${i}:${value}`} />)}
+      </div>
+    </div>
+  );
 }
 
 const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(<List />);
