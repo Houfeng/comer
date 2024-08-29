4a2c6f1045e8cda9e7ffa1c882a45a7389b376b6
zhanfeng.hzf <admin@xhou.net>
2024-08-29 19:05:57 +0800

    build script

--- packages/comer-runtime/task.config.mjs
+++ packages/comer-runtime/task.config.mjs
@@ -1,11 +1,19 @@
-import { task, $, version } from 'taskd';
+import { task, $ } from 'taskd';
+import cpy from 'cpy';
 
-console.warn('+++++++', { version })
+export const prebuild = task('Prebuild', async () => {
+  await $`dotnet build Comer.Runtime/Comer.Runtime.prebuild.csproj`;
+  await $`shopt -s globstar`;
+  await cpy(
+    'Comer.Runtime/obj/Debug/net8.0/generated/Comer.Runtime.Generator/*.g.cs',
+    'Comer.Runtime/gen/'
+  );
+});
 
-export const build = task('Build', async () => {
-  $`dotnet publish --ucr`;
+export const build = task('Build', [prebuild], async () => {
+  await $`dotnet publish Comer.Runtime/Comer.Runtime.publish.csproj --ucr `;
 });
 
 export const dev = task('Dev', [build], async () => {
-  $`node ./demo/demo1.js`;
+  await $`node ./demo/demo1.js`;
 });
