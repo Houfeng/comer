68a3188c635c1ba39be93d40da74cd8719daa5fc
zhanfeng.hzf <admin@xhou.net>
2024-09-06 15:11:47 +0800

    refine PropertiesManager

--- packages/comer-runtime/Comer.Runtime/src/Layouts/Vector.cs
+++ packages/comer-runtime/Comer.Runtime/src/Layouts/Vector.cs
@@ -7,7 +7,7 @@ public struct Vector {
   public double X { get; set; }
   public double Y { get; set; }
   public Vector(double x, double y) {
-    this.X = x;
-    this.Y = y;
+    X = x;
+    Y = y;
   }
 }
