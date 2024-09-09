c0e7e8756953cd6b8c10701420aa0d1010ed6f0c
zhanfeng.hzf <admin@xhou.net>
2024-09-10 00:25:35 +0800

    Texbox & Button

--- packages/comer-runtime/Comer.Runtime.Test/Program.cs
+++ packages/comer-runtime/Comer.Runtime.Test/Program.cs
@@ -10,7 +10,7 @@ public static class Program {
     Application.Init();
 
     var win = new Window();
-    win.SetProperty("Background", "#eeffee");
+    win.SetProperty("Background", "#FFF");
     win.Title = "Demo";
 
     var stack = new StackView();
