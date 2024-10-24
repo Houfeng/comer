cb92526214da012b11f0da594921f2342c0bd1cb
zhanfeng.hzf <admin@xhou.net>
2024-10-24 22:58:55 +0800

    build config

--- packages/comer/rollup.config.mjs
+++ packages/comer/rollup.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
-function createConfig(format, min = false, external = void 0, global = void 0) {
+function createConfig(format, min = false, external = void 0, globals = void 0) {
   const suffix = min ? 'min.js' : 'js';
   return defineConfig({
     input: './lib/index.js',
     output: {
       file: `./dist/comer.${format}.${suffix}`,
       format: format,
-      name: "Comer"
+      name: "Comer",
+      globals: globals,
     },
     external: external,
-    global: global,
     plugins: [
       resolve(),
       min ? terser() : void 0,
