24f47017452695106134268cb9878c63d8020dd9
zhanfeng.hzf <admin@xhou.net>
2024-10-24 22:53:05 +0800

    build config

--- packages/comer-dom/rollup.config.mjs
+++ packages/comer-dom/rollup.config.mjs
@@ -2,33 +2,17 @@ import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
-function createConfig(min = false) {
+function createConfig(format, min = false, external = void 0, global = void 0) {
   const suffix = min ? 'min.js' : 'js';
   return defineConfig({
     input: './lib/index.js',
-    output: [
-      // {
-      //   file: `./dist/comer-dom.es.${suffix}`,
-      //   format: 'esm'
-      // },
-      // {
-      //   file: `./dist/comer-dom.cjs.${suffix}`,
-      //   format: 'cjs'
-      // },
-      {
-        file: `./dist/comer-dom.umd.${suffix}`,
-        format: 'umd',
-        name: "ComerDOM",
-        globals: { comer: "Comer" }
-      },
-      {
-        file: `./dist/comer-dom.iife.${suffix}`,
-        format: 'iife',
-        name: "ComerDOM",
-        globals: { comer: "Comer" }
-      }
-    ],
-    external: ["comer"],
+    output: {
+      file: `./dist/comer-dom.${format}.${suffix}`,
+      format: format,
+      name: "ComerDOM"
+    },
+    external: external,
+    global: global,
     plugins: [
       resolve(),
       min ? terser() : void 0,
@@ -36,4 +20,11 @@ function createConfig(min = false) {
   });
 }
 
-export default [createConfig(false), createConfig(true)]
+export default [
+  createConfig('iife', false, ['comer'], { comer: 'Comer' }),
+  createConfig('iife', true, ['comer'], { comer: 'Comer' }),
+  createConfig('umd', false, ['comer'], { comer: 'Comer' }),
+  createConfig('umd', true, ['comer'], { comer: 'Comer' }),
+  createConfig('esm', false, ['comer', 'tslib']),
+  createConfig('cjs', false, ['comer', 'tslib']),
+]
--- packages/comer/rollup.config.mjs
+++ packages/comer/rollup.config.mjs
@@ -2,30 +2,17 @@ import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
-function createConfig(min = false) {
+function createConfig(format, min = false, external = void 0, global = void 0) {
   const suffix = min ? 'min.js' : 'js';
   return defineConfig({
     input: './lib/index.js',
-    output: [
-      // {
-      //   file: `./dist/comer.es.${suffix}`,
-      //   format: 'esm'
-      // },
-      // {
-      //   file: `./dist/comer.cjs.${suffix}`,
-      //   format: 'cjs'
-      // },
-      {
-        file: `./dist/comer.umd.${suffix}`,
-        format: 'umd',
-        name: "Comer"
-      },
-      {
-        file: `./dist/comer.iife.${suffix}`,
-        format: 'iife',
-        name: "Comer"
-      }
-    ],
+    output: {
+      file: `./dist/comer.${format}.${suffix}`,
+      format: format,
+      name: "Comer"
+    },
+    external: external,
+    global: global,
     plugins: [
       resolve(),
       min ? terser() : void 0,
@@ -33,4 +20,11 @@ function createConfig(min = false) {
   });
 }
 
-export default [createConfig(false), createConfig(true)]
+export default [
+  createConfig('iife', false),
+  createConfig('iife', true),
+  createConfig('umd', false),
+  createConfig('umd', true),
+  createConfig('esm', false, ['ober', 'tslib']),
+  createConfig('cjs', false, ['ober', 'tslib']),
+]
